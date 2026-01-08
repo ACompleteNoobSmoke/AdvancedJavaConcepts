29f1245c9eb47837f707a70a4aca04cfa93f2e28
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-01-08 05:33:48 -0600

    Worked on functional programming and combination function

--- src/main/java/Functional/Functional.java
+++ src/main/java/Functional/Functional.java
@@ -1,14 +1,21 @@
 package Functional;
 
+import com.sun.tools.javac.Main;
+
+import java.util.List;
 import java.util.function.Function;
 
 public class Functional {
 
     //Function - Takes in type and needs return type
     //Function - Uses apply method to complete the function
-    static Function<Integer, Integer> plusOne = (number) -> number + 1;
+    static Function<Integer, Integer> plusOne = number -> number + 1;
+    static Function<Integer, Integer> doubleNum = number -> number * 2;
+    static Function<Integer, Integer> combined = plusOne.andThen(doubleNum);
+
 
     public static void main(String[] args) {
-        System.out.println(plusOne.apply(10));
+//        System.out.println(plusOne.apply(10));
+        List.of(1,2,3,4,5,6,7,8,9,10).stream().map(x -> combined.apply(x)).forEach(System.out::println);
     }
 }
