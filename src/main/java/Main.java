aefbdbb3db4bc0b3b30e6849558c86e9afcfbeb9
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-01-14 06:24:34 -0600

    Worked on custom function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import Custom.EmailValidator;
+import Predicate.PredicateFunction;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
@@ -14,27 +18,34 @@ public class Main {
         return new Hotmail();
     }
 
-    private EmailSender getEmailSender(int choice) {
-        return switch (choice) {
-            case 1 -> getGmail();
-            case 2 -> getHotmail();
-            default -> null;
-        };
-    }
+//    private EmailSender getEmailSender(int choice) {
+//        return switch (choice) {
+//            case 1 -> getGmail();
+//            case 2 -> getHotmail();
+//            default -> null;
+//        };
+//    }
+
+
     public static void main(String[] args) {
 //        int choice = input.nextInt();
 //        EmailSender emailSender = new Main().getEmailSender(choice);
 //        if (emailSender != null) emailSender.sendEmail("");
-        List<String>  videoGames = List.of("God of War", "Gears of War", "Devil May Cry 3");
-        videoGames.forEach(System.out::println);
-        int number = plusOne(1);
+//        List<String>  videoGames = List.of("God of War", "Gears of War", "Devil May Cry 3");
+//        videoGames.forEach(System.out::println);
+//        int number = plusOne(1);
+        List<String> emails = PredicateFunction.getEmails();
+        EmailValidator emailValidator = new EmailValidator();
+        emails.stream().filter(emailValidator).forEach(System.out::println);
+
+
     }
 
-    static Function<Integer, Integer> plusOne(Integer number) {
-        return integer -> integer + number;
-    }
-
-    static int plusOne(int a) {
-        return a + 1;
-    }
+//    static Function<Integer, Integer> plusOne(Integer number) {
+//        return integer -> integer + number;
+//    }
+//
+//    static int plusOne(int a) {
+//        return a + 1;
+//    }
 }
---
+++ src/main/java/Supplier/SupplierFunction.java
@@ -0,0 +1,16 @@
+package Supplier;
+
+import Model.Person;
+
+import java.util.function.Supplier;
+
+public class SupplierFunction {
+
+    private static Supplier<Person> personSupplier = () -> new Person("Osaretin", "Mecahinc");
+
+    static void main() {
+        System.out.println(personSupplier.get());
+    }
+
+
+}
