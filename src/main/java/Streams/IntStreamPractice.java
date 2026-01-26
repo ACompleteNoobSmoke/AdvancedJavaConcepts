0a45e808df2536585e59d7562b0afcee4b1190aa
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-01-26 06:01:15 -0600

    Iterated through a list using IntStream

--- src/main/java/Streams/IntStreamPractice.java
+++ src/main/java/Streams/IntStreamPractice.java
@@ -1,6 +1,12 @@
 package Streams;
 
+import Custom.NewPeopleSuppler;
+import Model.NewPeople;
+import Model.Person;
+
+import java.util.List;
 import java.util.stream.IntStream;
+import static Custom.NewPeopleSuppler.newPeopleSupplier;
 
 public class IntStreamPractice {
 
@@ -12,8 +18,15 @@ public class IntStreamPractice {
         intstream.forEach(System.out::println);
     }
 
+    private void rangeIteratingLists() {
+        List<NewPeople> people = newPeopleSupplier.get();
+        IntStream intstream = IntStream.range(0, people.size());
+        intstream.forEach(i -> System.out.println(people.get(i)));
+    }
+
     public static void main(String[] args) {
         IntStreamPractice intStreamPractice = new IntStreamPractice();
         intStreamPractice.rangePractice();
+        intStreamPractice.rangeIteratingLists();
     }
 }
