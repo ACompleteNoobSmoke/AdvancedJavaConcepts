62d95dacbe2772649664149d6f9e3df130422972
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-02-03 06:46:13 -0600

    Worked on transformation again

--- src/main/java/Streams/Transformation.java
+++ src/main/java/Streams/Transformation.java
@@ -4,12 +4,24 @@ import Beans.PersonDTO;
 import Model.Gender;
 import Model.NewPeople;
 
-import java.util.List;
-import java.util.OptionalDouble;
+import java.util.*;
 
 import static Custom.NewPeopleSuppler.newPeopleSupplier;
 
 public class Transformation {
+
+    private List<List<String>> getNames() {
+        List<List<String>> result = new ArrayList<>();
+        List<NewPeople> people = newPeopleSupplier.get();
+        for (int i = 0; i < 3; i++) {
+            List<String> name = new ArrayList<>();
+            for (int j  = i; j < i + 3; j++) {
+                name.add(people.get(j).firstName());
+            }
+            result.add(name);
+        }
+        return result;
+    }
 
     public void transformWithMap() {
         List<NewPeople> people = newPeopleSupplier.get();
@@ -25,8 +37,24 @@ public class Transformation {
                 .average();
         if (averageAge.isPresent())
             System.out.println("Average age of females: " + averageAge.getAsDouble());
+    }
 
+    public void reduce() {
+        int[] integers = {1, 2, 3, 4, 5, 6, 7, 8, 19, 20, 123423, 22};
+        int sum = Arrays.stream(integers).reduce(0, ((left, right) -> left + right));
+        System.out.println("Sum of all integers: " + sum);
+    }
 
+    public void withoutFlatMap() {
+        List<List<String>> names = getNames();
+        List<String> result = new ArrayList<>();
+        names.forEach(result::addAll);
+        result.forEach(System.out::println);
+    }
+
+    public void withFlatMap() {
+        List<String> names = getNames().stream().flatMap(Collection::stream).toList();
+        names.forEach(System.out::println);
     }
 
     public static void main(String[] args) {
@@ -34,5 +62,11 @@ public class Transformation {
         transformation.transformWithMap();
         System.out.println();
         transformation.transformToAverageFemaleAge();
+        System.out.println();
+        transformation.reduce();
+        System.out.println();
+        transformation.withoutFlatMap();
+        System.out.println();
+        transformation.withFlatMap();
     }
 }
